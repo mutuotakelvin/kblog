bf0e3d7a1c69484fbbbdbd44932271b5fefe2c02
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2021-11-23 00:39:07 +0300

    work on updating the post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from django.db import models
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
@@ -15,3 +16,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
